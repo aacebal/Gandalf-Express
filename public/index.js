b1559f0deaee832b361f0701f657ede14b34b603
Adel Acebal <adelacebal@yahoo.com>
2017-04-15 09:53:07 -0400

    done

--- public/index.js
+++ public/index.js
@@ -3,16 +3,19 @@ var background = true;
 $('document').ready(function(){
 
 $('html').on('click', function(){
+  setTimeout(function () {
+    if (background) {
+      $('body').css({'backgroundImage': 'url(../images/gandalf-home.jpg)'});
+      $('.container').removeClass('col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xl-offset-6');
+      background = false;
+    }
+    else {
+      $('body').css({'backgroundImage': 'url(../images/gandalf_2.jpg)'});
+      $('.container').addClass('col-sm-offset-6 col-md-offset-6 col-lg-offset-6 col-xl-offset-6');
+      background = true;
+    }
+  }, 10);
 
-
-  if (background) {
-    $('body').css({'backgroundImage': 'url(../images/gandalf-home.jpg)'});
-    background = false;
-  }
-  else {
-    $('body').css({'backgroundImage': 'url(../images/gandalf_2.jpg)'});
-    background = true;
-  }
 
 });
 
